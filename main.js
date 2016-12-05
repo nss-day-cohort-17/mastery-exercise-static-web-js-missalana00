f181943ca9262bc7a0883aa2d122cbe56fc04320
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2016-12-05 00:45:50 -0600

    Completed self-identified pre-step 1

--- main.js
+++ main.js
@@ -4,23 +4,27 @@ It is absolutely critical that you read each of these requirements because as a 
 you will be expected to completely understand what you are expected to do.
 If you submit code that does not fulfill the requirements, you will be sent to your room with no dinner.
 
-Create a tree function that should build a pine tree out of a character in the Chrome dev tools console.
+CREATE A TREE FUNCTION that should build a pine tree out of a character in the Chrome dev tools console.
 
-It accepts a single object as an argument. The object should have two key/value pairs.
+It accepts a single OBJECT as an argument. The object should have TWO KEY/VALUE PAIRS:
 
-A key that specifies the height of the pine tree.
-The value for the height of the tree should be from user input in a <input type="text"> field in the DOM.
-A key that specifies which character to use to build the pine tree.
-The character to use should be from user input in a <input type="text"> field in the DOM.
+1. A key that specifies the height of the pine tree.
+The value for the height of the tree should be from USER INPUT in a <input type="text"> field in the DOM.
+
+2. The character to use should be from user input in a <input type="text"> field in the DOM.
+
 Once the user enters in a number, and a character, the user can either then just press the enter key
-(as long as the cursor is in one of the input fields), or click a button that is labeled "Grow your tree" and
-the tree should be shown in the console. This requires you to add an event listener to the button,
-as well as an event listener for the enter/return key.
+(as long as the cursor is in one of the input fields), or CLICK A BUTTON that is labeled "Grow your tree" and
+the tree should be shown in the console.
+
+This requires you to ADD AN EVENT LISTENER TO THE BUTTON,
+as well as ADD AN EVENT LISTENER FOR THE ENTER/RETURN KEY.
+
 
 If either of the input fields does not have a value in it when the user presses the enter key, or presses
-the button, then display an alert stating that both fields must have a value.
+the button, then display an alert stating that both fields must have a value (VALIDATE INFORMATION).
 
-Grow your tree
+Grow your tree:
 
 Example
 
@@ -34,3 +38,11 @@ Here's what the pine tree should look like when you specify a height of 7, and u
  ***********
 *************
 */
+
+
+// Pre-Step 1: Write the HTML that creates the two text feilds for
+// the user to enter the height and character of their tree.
+// Additionally, create a button for the user to click that says, "Grow your tree."
+
+
+// Step 1: You need variables that reference elements in the DOM
