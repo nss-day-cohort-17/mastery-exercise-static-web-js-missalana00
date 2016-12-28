bd9237fd0b35d3b50155aa3c5b014ba12c42f7ce
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2016-12-28 11:35:51 -0600

    Completed the-static-web mastery exercise

--- main2.js
+++ main2.js
@@ -15,22 +15,33 @@ function getTreeInfo () {
     } else {
         alert("You must enter a value in both fields");
     }
-
 }
 
 
 function createTree (object) {
 
-var output = ' ';  /* Note: You have to declare var before filling it. You also need to give it
+var outputTree = ' ';  /* Note: You have to declare var before filling it. You also need to give it
 an empty string as a place holder. */
 
 for (var i = 0; i < object.height; i++ ) {
-
-   console.log(object.char);
+outputTree = " ".repeat(object.height - 1 - i)
+outputTree += object.char.repeat(i * 2 + 1)
+   console.log(outputTree);
   }
 }
 
-
-
-
 growButton.addEventListener("click", getTreeInfo)
+
+userInputHeight.addEventListener("keypress", function(e) {
+
+    if (e.key === "Enter") {
+    getTreeInfo();
+    }
+});
+
+userInputChar.addEventListener("keypress", function(e) {
+
+    if (e.key === "Enter") {
+    getTreeInfo();
+    }
+});
